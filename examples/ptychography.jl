f80bd88ec8975ba76b9c61ad002fb3fa8503ef0e
RainerHeintzmann <heintzmann@gmail.com>
2026-07-06 12:34:00 +0200

    example update

--- examples/ptychography.jl
+++ examples/ptychography.jl
@@ -20,37 +20,56 @@ function simulate_data(obj, prop_od, prop_ds, diffusor)
     abs2.(atsensor)
 end
 
+"""
+    get_diffusors(screen, shifts)
+
+returns shifted diffusors
+"""
+function get_diffusors(sz, screen, shifts)
+    midpos = size(screen).÷2 .+1
+    shifted_screen = []
+    for myshift in eachslice(shifts, dims=1)
+        push!(shifted_scree, select_region(screen, sz; center=ft_center_diff = midpos .+ Tuple(round.(Int, myshift))))
+    end
+    return cat(shifted_screen..., dims=3)
+    # not working in Zygote either:
+    # return cat((select_region(screen, sz; center=ft_center_diff = midpos .+ Tuple(round.(Int, myshift))) for myshift in eachslice(shifts, dims=1))..., dims=3)
+    # return cat((select_region_view(shift(screen, myshift), sz) for myshift in shifts)..., dims=3)
+end
+
 function fwd_model(params)
     obj = params(:obj)
     prop_od = params(:prop_od)
     prop_ds = params(:prop_ds)
-    diffusor = params(:diffusor)
+    screen = params(:screen)
+    shifts = params(:shifts)
+
+    diffusor = get_diffusors(size(obj)[1:2], screen, shifts)
 
     atdiffusor = conv_psf(obj, prop_od, (1,2))
     atdiffusor = atdiffusor .* diffusor
     atsensor = conv_psf(atdiffusor, prop_ds, (1,2))
-    abs2.(atsensor)
+    abs2.(atsensor)  # return the intensity at the sensor
 end
 
-
-function get_diffusor(sz, shifts; σ_mag=1.0, σ_phase=1000.0, fmax = 0.1)
+function get_diffusor(sz; σ_mag=1.0, σ_phase=1000.0, fmax = 0.1)
     bigsz = 2 .*sz
     amp = (1 .+ σ_mag.*randn((bigsz)...)).* cis.(2π.*rand(bigsz...))
-    screen = ComplexF32.(ift(ft(amp).* disc(bigsz, fmax; scale=ScaFT)))
-    diffusor=zeros(eltype(screen), sz..., length(shifts))
-    for (slice, myshift) in zip(eachslice(diffusor, dims=3), shifts)
-        slice .= select_region_view(shift(screen, myshift), sz)
-    end
-    return diffusor
+    return ComplexF32.(ift(ft(amp).* disc(bigsz, fmax; scale=ScaFT)))
 end
+
 
 function main()
     mag = Float32.(testimage("resolution_test_512"))
-    phase = 2π .* mag[:,end:-1:1] .- π
-    obj = mag .* cis.(phase)
+    mag = select_region(mag, (256, 256))
+    # scansz = (10,10)  # (10,10) scan -> 100 images
+    scansz = (5,5)  # (5,5) scan -> 25 images
 
-    v = @vp obj
-    set_gamma(1.0)
+    phase = 2π .* mag[:,end:-1:1] .- π
+    obj = ComplexF32.(mag .* cis.(phase))
+
+    # v = @vp obj
+    # set_gamma(1.0)
 
     λ=0.661
     NA = 0.05
@@ -63,34 +82,47 @@ function main()
     prop_ds = apsf((size(obj)..., 3), pp; sampling=samp_ds)[:,:,3,1]
     # @vt prop_od prop_ds
 
-    scansz = (10,10)  # (10,10)
 
     wiggle = 0.5
     scanmag = 20.0
-    ctr(x) = scanmag.*Tuple(x .- (scansz .÷ 2 .+1) .+ wiggle.*rand(2))
-    shifts = ctr.(Tuple.(CartesianIndices(scansz)))
-    diffusor = get_diffusor(size(obj), shifts[:]);
+    ctr(x) = Float32.(scanmag.*Vector(x .- (scansz .÷ 2 .+1) .+ wiggle.*rand(2)))
+    shifts_r = (ctr.(Tuple.(CartesianIndices(scansz))))[:]
+    shifts = permutedims(cat(shifts_r..., dims=2), (2,1))
+    screen = get_diffusor(size(obj));
+    # diffusor = get_diffusors(size(obj), screen, shifts)
+
     # @vp diffusor
 
     # int_sensor = simulate_data(obj, prop_od, prop_ds, diffusor)
 
     # @vt int_sensor
-# --------------- simulation done, lets solve the inverse problem -------
 
-    start_val = (obj=obj, prop_od=Fixed(prop_od), prop_ds=Fixed(prop_ds), diffusor=Fixed(diffusor))
+    start_val = (obj=obj, prop_od=Fixed(prop_od), prop_ds=Fixed(prop_ds), screen=Fixed(screen), shifts=Fixed(shifts))
     start_vals, fixed_vals, forward, backward, get_fit_results = create_forward(fwd_model, start_val)
 
     pimg = forward(start_vals)
     nphotons = 100;
     nimg = Float32.(poisson(pimg./mean(pimg) .* nphotons))
 
-    const_obj = ComplexF32.(nimg[:,:,1:1] .* 0 .+ mean(sqrt.(nimg)))
-    start_val = (obj=const_obj, prop_od=Fixed(prop_od), prop_ds=Fixed(prop_ds), diffusor=Fixed(diffusor))
+    # start_val = (obj=const_obj, prop_od=Fixed(prop_od), prop_ds=Fixed(prop_ds), diffusor=Fixed(diffusor))
  
-    use_cuda = true
+# --------------- simulation done, lets solve the inverse problem -------
+
+    const_obj = ComplexF32.(nimg[:,:,1:1] .* 0 .+ mean(sqrt.(nimg)))
+
+    start_val = (obj=const_obj, prop_od=Fixed(prop_od), prop_ds=Fixed(prop_ds), screen=Fixed(screen), shifts=Fixed(shifts))
+
+    const_screen = screen .* 0 .+ 0.5
+    start_val = (obj=cu(const_obj), prop_od=Fixed(cu(prop_od)), prop_ds=Fixed(cu(prop_ds)), screen=const_screen)
+
+    use_cuda = false # true
+    # use_cuda = true
     # cu = (x) -> CuArray(x)
     if (use_cuda)
-        start_val = (obj=cu(const_obj), prop_od=Fixed(cu(prop_od)), prop_ds=Fixed(cu(prop_ds)), diffusor=Fixed(cu(diffusor)))        
+        # optimize only obj with the given diffusor:
+        start_val = (obj=cu(const_obj), prop_od=Fixed(cu(prop_od)), prop_ds=Fixed(cu(prop_ds)), screen=Fixed(cu(screen)), shifts=Fixed(cu(shifts)))
+        # optimize both obj and diffusor:
+        start_val = (obj=cu(const_obj), prop_od=Fixed(cu(prop_od)), prop_ds=Fixed(cu(prop_ds)), screen=cu(const_screen), shifts=Fixed(cu(shifts)))
         torecon = cu(nimg)
     else
         torecon = nimg
